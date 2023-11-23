1a8f4d40730835f661ae6d9dcdc85382b3ff16cf
Martin Kihungi <mwangim4786@gmail.com>
2023-11-23 05:29:38 +0300

    console.py 1

--- console.py
+++ console.py
@@ -2,8 +2,10 @@
 """ Console Module """
 import cmd
 import sys
+from shlex import split
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
+from datetime import datetime
 from models.user import User
 from models.place import Place
 from models.state import State
