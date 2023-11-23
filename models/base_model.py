5112ae508ff147d2e5b41f78d6cdb924353ca8b7
Martin Kihungi <mwangim4786@gmail.com>
2023-11-23 05:34:31 +0300

    base_model.py 4

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,8 @@ class BaseModel:
         """Instatntiates a new BaseModel"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             
         else:
             for key, value in kwargs.items():
