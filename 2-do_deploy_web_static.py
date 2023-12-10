e2ba6f38f42ab818fe7991c03697cc3bd3df1d01
Martin Kihungi <mwangi4786@gmail.com>
2023-12-10 12:19:27 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,25 +20,30 @@ def do_deploy(archive_path):
 
                 # create target directory, copy datetime to variable timestamp
                 timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/releases/web_static_{}/'.format(timestamp))
+                run('sudo mkdir -p /data/web_static/releases/web_static_{}
+/'.format(timestamp))
 
                 # uncompress archive file to set dir. and and delete .tgz file
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C /data/
+web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
                 # remove archive file
                 run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
                 # move contents into host web_static dir.
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+                run('sudo mv /data/web_static/releases/web_static_{}/
+web_static/* /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
                 # remove thw extra web_static dir.
-                run('sudo rm -rf /data/web_static/releases/web_static_{}/web_static'.format(timestamp))
+                run('sudo rm -rf /data/web_static/releases/web_static_{}/
+web_static'.format(timestamp))
 
                 # delete existing symbolic link
                 run('sudo rm -rf /data/web_static/current')
 
                 # create new symbolic link
-                run('sudo ln -s /data/web_static/releases/web_static_{}/ /data/web_static/current'.format(timestamp))
+                run('sudo ln -s /data/web_static/releases/web_static_{}/ 
+/data/web_static/current'.format(timestamp))
         except:
                 return False
 
