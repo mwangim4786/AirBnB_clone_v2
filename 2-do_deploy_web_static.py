4533b20e57df790d93c79dfd91af9c8ad2734959
Martin Kihungi <mwangi4786@gmail.com>
2023-12-10 13:25:14 -0800

    2-do_deploy_web_static.py 7

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,7 +26,8 @@ releases/web_static_{}/'.format(timestamp))
 
                 # uncompress archive file to set dir. and and delete .tgz file
                 run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+/data/web_static/releases/web_static_{}/'
+                    .format(timestamp, timestamp))
 
                 # remove archive file
                 run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
@@ -37,7 +38,8 @@ releases/web_static_{}/'.format(timestamp))
 
                 # remove thw extra web_static dir.
                 run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'.format(timestamp))
+web_static_{}/web_static'
+                    .format(timestamp))
 
                 # delete existing symbolic link
                 run('sudo rm -rf /data/web_static/current')
