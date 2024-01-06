d4668d6da805dba2898f55832140796b1e9c13b4
mwangim4786 <mwangim4786@gmail.com>
2024-01-06 13:32:49 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -18,6 +18,6 @@ def hbnb_flask():
     """
     return 'HBNB'
 
-if __name__ == '__main__':i
+if __name__ == '__main__':
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
