ede9e5250af39d5bad984a9f55cbec682dc976b6
mwangim4786 <mwangim4786@gmail.com>
2024-04-21 08:55:23 -0700

    1-hbnb_route.py correction 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,9 +4,10 @@
 
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_flask():
     """Return string Hello HBNB! when route is queried
     """
