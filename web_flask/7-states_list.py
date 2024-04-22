38975cb6635bac7cf5f4117ed218e4f9f1c63140
mwangim4786 <mwangim4786@gmail.com>
2024-04-22 03:31:00 -0700

    7-7 corrections

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,17 +8,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states_list')
-def states_list():
-    """Render template with states
-    """
-    path = '7-states_list.html'
-    states = storage.all(State)
-    # sort States object by name, from A-Z
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, sorted_states=sorted_states)
-
-
 @app.teardown_appcontext
 def app_teardown(arg=None):
     """Clean-up session
@@ -26,6 +15,20 @@ def app_teardown(arg=None):
     storage.close()
 
 
+@app.route('/states_list')
+def states_list():
+    """Render template with states
+    """
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort States object by name, from A-Z
+    sorted_states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    #sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
+
+
+
+
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5002)
